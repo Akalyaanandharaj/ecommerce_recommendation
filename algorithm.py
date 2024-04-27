a64865e93988be17ada9474b30c114b35b454304
Akalya Anandharaj <anandharajakalya@gmail.com>
2024-04-27 15:56:21 +0800

    return webp files

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from PIL import Image
+from io import BytesIO
+import requests, ast
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import random, pickle
@@ -8,6 +11,17 @@ import random, pickle
 df = pd.read_csv("data/ecommerce_data.csv")
 df['description_cleaned'] = df['description'].replace(r'[^a-zA-Z0-9\s]', '', regex=True)
 df['name_cleaned'] = df['name'].replace(r'[^a-zA-Z0-9\s]', '', regex=True)
+
+def string_to_list(string):
+    return ast.literal_eval(string)
+df['image'] = df['images'].apply(string_to_list)
+
+def get_first_value(lst):
+    if isinstance(lst, list) and len(lst) > 0:
+        return lst[0]
+    else:
+        return None
+df['img_webp'] = df['image'].apply(get_first_value)
 
 # Combine description, occasion, and age group into a single text for TF-IDF Vectorizer
 df['Combined'] = df['description_cleaned'] + df['name_cleaned']
@@ -33,7 +47,7 @@ df['Combined'] = df['description_cleaned'] + df['name_cleaned']
 #     style = input("Enter the Style (boho/vintage/modern) : ").strip().lower()
 #     color = input("Enter the color  : ").strip().lower()
 #     return occasion, material, style, color
-#
+# # #
 def get_recommendations(occasion, age_group, description, cosine_sim, df,tfidf_vectorizer, top_n=3):
     # Create a combined query
     query = description + ' ' + occasion + ' ' + age_group
@@ -52,26 +66,26 @@ def get_recommendations(occasion, age_group, description, cosine_sim, df,tfidf_v
     sim_scores = sim_scores[:top_n]
     perfume_indices = [i[0] for i in sim_scores]
 
-    results = df[['name', 'url']].iloc[perfume_indices].reset_index(drop=True)
+    results = df[['name', 'img_webp']].iloc[perfume_indices].reset_index(drop=True)
     return results.to_json()
-
-# # Load TF-IDF Vectorizer
-# with open('tfidf_vectorizer.pkl', 'rb') as f:
-#     tfidf_vectorizer = pickle.load(f)
-#
-# # Load Cosine Similarity Matrix
-# with open('cosine_sim.pkl', 'rb') as f:
-#     cosine_sim = pickle.load(f)
-#
-# # Get user input
-# occasion, age_group, description = get_user_input()
-#
-# print (occasion, age_group, description)
-#
-# # Get perfume recommendations
-# recommendations = get_recommendations(occasion, age_group, description, cosine_sim, df)
-#
-# print(recommendations)
-#
-#
-#
+# #
+# # # # Load TF-IDF Vectorizer
+# # with open('tfidf_vectorizer.pkl', 'rb') as f:
+# #     tfidf_vectorizer = pickle.load(f)
+# #
+# # # Load Cosine Similarity Matrix
+# # with open('cosine_sim.pkl', 'rb') as f:
+# #     cosine_sim = pickle.load(f)
+# #
+# # # Get user input
+# # occasion, age_group, description = get_user_input()
+# #
+# # print (occasion, age_group, description)
+# #
+# # # Get perfume recommendations
+# # recommendations = get_recommendations(occasion, age_group, description, cosine_sim, df)
+# #
+# # print(recommendations)
+# #
+# #
+# #
